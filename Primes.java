7c97519faa265a45cd9a03fe1cc98df72c031a92
TomE137 <50273189+TomE137@users.noreply.github.com>
2024-12-03 18:46:29 +0200

    Prime updated to v001

--- Primes.java
+++ Primes.java
@@ -1,5 +1,41 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        final int high = Integer.parseInt(args[0]);
+        String numberarray = "2"; // numberarray will be used as an array to store all the prime numbers that are found within the given range
+                                  // the use of a string for storage will be more efficient in this case as i can add items to it during the run
+        boolean prime = true;
+        int charsize = 1, counter =1; //counter will count the amount of numbers within the given range that are prime and charsize will count the                                               amount of digits within the prime number that needs to be extracted from the numberarray string
+        System.out.println("Prime numbers up to " + high + ":");
+        if (high >= 2) {
+            System.out.println("2");      
+        } else {
+            counter--;
+            numberarray="";
+        }
+        // i+=2 as all the even numbers are not prime
+        for (int i = 3; i <= high; i+=2) {
+            prime = true;
+            charsize = 1; // default the digit size for an item in numberarray to 1
+            for(int j = 0; j < numberarray.length(); j++) {
+                // checks whether the next char of the string will be smaller than the last. this will only occur when then next number within the                          has a larger digit count than the previous number (i.e: 7 and 11, 97 and 101...)  
+                if(numberarray.length()!=1 && j!=0 && numberarray.charAt(j-1)>numberarray.charAt(j)) {
+                    charsize++;
+                }
+                if(i % Integer.parseInt(numberarray.substring(j, j + charsize)) == 0) {
+                    prime = false;
+                    break;
+                }
+                // prevents an out of bounds error when checking multiple digits within the string
+                if(j + charsize + 1 > numberarray.length()-1) {
+                    break;
+                }
+            }
+            if(prime) {
+                System.out.println(i);
+                numberarray += i;
+                counter++;
+            }
+        } 
+        System.out.println("There are " + counter + " prime numbers between 2 and " + high + " (" + (int) (100 * (double) numberarray.length() / high) + "% are primes)");
     }
 }
